3892a0ca72e7020aebd3d14d678da0ba06adbd38
John Paul Hayes II <bwackwat@github.com>
2016-11-03 13:31:28 -0700

    Refactored parser for an unlimited stream of EBML elements. Verbose output is very nice. Things make more sense.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 #include <unistd.h>
 
-#define BUFSIZE 1000000 // 1MB
+#define BUFSIZE 8192 // 8KB
 
 enum ebml_element_type {
 	MASTER,
@@ -28,7 +28,7 @@ public:
 	uint64_t get_uint(){
 		uint64_t value = 0;
 		value = data[width - 1];
-		for(int i  = width - 1; i > 0; --i){
+		for(int i = width - 1; i > 0; --i){
 			value += ((uint64_t)data[i - 1] << ((width - i) * 8));
 		}
 		return value;
@@ -70,32 +70,185 @@ ebml_element* get_element(std::array<uint8_t, 4> id, uint8_t level){
 	return 0;
 }
 
+bool verbose = false;
+
+typedef std::ostream& (*manip) (std::ostream&);
+class basic_log{};
+template <class T> basic_log& operator<< (basic_log& l, const T& x){
+	std::cout << x;
+	return l;
+}
+basic_log& operator<< (basic_log& l, manip manipulator){
+	std::cout << manipulator;
+	return l;
+}
+class verbose_log{};
+template <class T> verbose_log& operator<< (verbose_log& l, const T& x){
+	if(verbose)
+		std::cout << x;
+	return l;
+}
+verbose_log& operator<< (verbose_log& l, manip manipulator){
+	if(verbose)
+		std::cout << manipulator;
+	return l;
+}
+basic_log log;
+verbose_log vlog;
+
 int main(int argc, char** argv){
 	int len, mask;
 	uint8_t buffer[BUFSIZE];
-
-	int total_bytes = 0;
-	int from_bytes = 0;
-	int to_bytes = 0;
-
-	bool verbose = false;
-
-	simple_vint* e_id;
-	simple_vint* e_size;
-	simple_vint* e_data;
+	std::bitset<8> bits;
 
 	for(int i = 0; i < argc; i++){
-		if((std::strcmp(argv[i], "-f") == 0) && argc > i){
-                        from_bytes = std::stoi(argv[i + 1]);
-                }else if((std::strcmp(argv[i], "-t") == 0) && argc > i){
-                        to_bytes = std::stoi(argv[i + 1]);
-                }else if((std::strcmp(argv[i], "-l") == 0) && argc > i){
-                        to_bytes = from_bytes + std::stoi(argv[i + 1]);
-                }else if(std::strcmp(argv[i], "-v") == 0){
+                if(std::strcmp(argv[i], "-v") == 0){
                         verbose = true;
                 }
 	}
 
+	while(1){
+		// Get EBML Element ID first byte.
+		if((len = read(STDIN_FILENO, buffer, 1)) < 0){
+			std::cout << "Uh oh, read first id byte error!\n";
+			break;
+		}else if(len == 0){
+			vlog << "DONE!" << std::endl;
+			break;
+		}
+
+		bits = std::bitset<8>(buffer[0]);
+		vlog << "Element ID First Byte: " << bits << std::endl;
+
+		simple_vint id;
+		id.width = 1;
+		mask = 0x80;
+		// Get EBML Element ID vint width.
+		while(!(buffer[0] & mask)){
+			mask >>= 1;
+			id.width++;
+		}
+
+		id.data[0] = buffer[0];
+		// Get EBML Element ID vint data.
+		if((len = read(STDIN_FILENO, buffer, id.width - 1)) != id.width - 1){
+			std::cout << "Uh oh, read id data error!\n";
+			break;
+		}
+		vlog << "Element ID Bytes: " << bits;
+		// Get EBML Element ID.
+		for(int i = 1; i < id.width; ++i){
+			id.data[i] = buffer[i - 1];
+			bits = std::bitset<8>(buffer[i]);
+			vlog << ' ' << bits;
+		}
+		vlog << std::endl;
+		if(verbose){
+			vlog << "Element ID: 0x";
+			for(int i = 0; i < id.width; ++i){
+				vlog << std::hex << (int)id.data[i];
+			}
+			vlog << std::endl;
+		}
+
+		// Get EBML Element Size first byte.
+		if((len = read(STDIN_FILENO, buffer, 1)) != 1){
+			std::cout << "Uh oh, read first size byte error!\n";
+			break;
+		}
+
+		bits = std::bitset<8>(buffer[0]);
+		vlog << "Element Size First Byte: " << bits << std::endl;
+
+		simple_vint size;
+		size.width = 1;
+		mask = 0x80;
+		// Get EBML Element Size vint width.
+		while(!(buffer[0] & mask)){
+			mask >>= 1;
+			size.width++;
+		}
+
+		buffer[0] ^= mask;
+		size.data[0] = buffer[0];
+		// Get EBML Element Size vint data.
+		if((len = read(STDIN_FILENO, buffer, size.width - 1)) != size.width - 1){
+			std::cout << "Uh oh, read id data error!\n";
+			break;
+		}
+		bits = std::bitset<8>(size.data[0]);
+		vlog << "Element Size Bytes: " << bits;
+		// Get EBML Element Size.
+		for(int i = 1; i < size.width; ++i){
+			size.data[i] = buffer[i - 1];
+			bits = std::bitset<8>(buffer[i]);
+			vlog << ' ' << bits;
+		}
+		vlog << std::endl;
+		vlog << "Element Size: " << std::dec << size.get_uint() << std::endl;
+
+		// Specification for ID lookup.
+		ebml_element* e = get_element(
+			{{id.data[0], id.data[1], id.data[2], id.data[3]}},
+			id.width);
+
+		vlog << "--------------------------------------------------------" << std::endl;
+		if(e != 0){
+			if(e->type != MASTER){
+				// Get EBML Element Data, parse it.
+				uint64_t data_len = size.get_uint();
+				if((len = read(STDIN_FILENO, buffer, data_len) != data_len)){
+					std::cout << "Uh oh, could not read all the data!\n";
+					break;
+				}
+				std::cout << e->name << ": ";
+				if(e->type == STRING || e->type == UTF8){
+					for(int i = 0; i < data_len; ++i){
+						std::cout << buffer[i];
+					}
+				}else if(e->type == BINARY){
+					for(int i = 0; i < data_len; ++i){
+						// I'll only care about the first 32 binary bytes.
+						if(i == 32 && !verbose){
+							std::cout << "...";
+							break;
+						}
+						std::cout << std::hex << (int)buffer[i];
+					}
+				}else if(e->type == UINT){
+					simple_vint data;
+					data.width = 0;
+					for(int i = 0; i < data_len; ++i){
+						data.data[i] = buffer[i];
+						data.width++;
+					}
+					std::cout << std::dec << data.get_uint();
+				}else if(e->type == INT){
+					simple_vint data;
+					data.width = 0;
+					for(int i = 0; i < data_len; ++i){
+						data.data[i] = buffer[i];
+						data.width++;
+					}
+					std::cout << std::dec << data.get_int();
+				}else{
+					for(int i = 0; i < data_len; ++i){
+						std::cout << std::hex << (int)buffer[i];
+					}
+				}
+				std::cout << std::endl;
+			}else{
+				// Master data is actually just more elements, continue.
+				std::cout << " - " << e->name << std::endl;
+			}
+		}else{
+			std::cout << "UNKNOWN ELEMENT: ";
+		}
+		vlog << "--------------------------------------------------------" << std::endl;
+	}
+
+
+/*
 	if((len = read(STDIN_FILENO, buffer, BUFSIZE)) == -1){
 		std::cout << "Uh oh, read error!\n";
 		return 1;
@@ -241,6 +394,6 @@ int main(int argc, char** argv){
 	}
 
 	total_bytes += len;
-
+*/
 	return 0;
 }
